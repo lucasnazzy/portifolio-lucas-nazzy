2da7fcf9ce3dd2d2075504c47548466bedbc07c8
Lucas Nazzini <lucasnazzy@doubleit.com.br>
2022-03-20 18:05:18 -0300

    ajustes no código

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 window.addEventListener("scroll", reveal);
 window.addEventListener("scroll", revealSkills);
 window.addEventListener("scroll", firstSkills);
-window.addEventListener("scroll", secondSkills);
-window.addEventListener("scroll", thirdSkills);
 
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
@@ -36,36 +34,6 @@ function revealSkills() {
 }
 function firstSkills() {
   var reveals = document.querySelectorAll(".first-lineskills");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
-
-    if (revealtop < windowheight - revealpoint) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-function secondSkills() {
-  var reveals = document.querySelectorAll(".second-lineskills");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
-
-    if (revealtop < windowheight - revealpoint) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-function thirdSkills() {
-  var reveals = document.querySelectorAll(".third-lineskills");
 
   for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
