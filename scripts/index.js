7c30493c3bbe4377382a61cd56dc8b2eaa37ee4a
Lucas Nazzini <lucasnazzy@doubleit.com.br>
2022-03-20 23:54:50 -0300

    ajustes no código

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 window.addEventListener("scroll", reveal);
 window.addEventListener("scroll", revealSkills);
 window.addEventListener("scroll", firstSkills);
+window.addEventListener("scroll", myTrajectoryTransition);
+window.addEventListener("scroll", myFormationTransition);
 
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
@@ -34,6 +36,36 @@ function revealSkills() {
 }
 function firstSkills() {
   var reveals = document.querySelectorAll(".first-lineskills");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+function myTrajectoryTransition() {
+  var reveals = document.querySelectorAll(".my-trajectory-transition");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+function myFormationTransition() {
+  var reveals = document.querySelectorAll(".my-formation-transition");
 
   for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
